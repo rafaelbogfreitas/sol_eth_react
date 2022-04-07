fd3ac5f3faa2ed3531fc3feee53fbcf0bbce181b
Rafael Freitas <rafaelborges1985@hotmail.com>
2022-04-06 23:06:58 -0300

    Create base faucet contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,6 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+  address[] public funders;
+
   receive() external payable {}
-  function addFunds() external payable {}
+
+  function addFunds() external payable {
+    bool existentFunder = false;
+
+    for (uint256 index = 0; index < funders.length; index++) {
+      if(msg.sender == funders[index]) {
+        existentFunder = true;
+      }
+    }
+
+    if(!existentFunder) {
+      funders.push(msg.sender);
+    }
+  }
+
+  function getFunders() external view returns(address[] memory) {
+    return funders;
+  }
 }
